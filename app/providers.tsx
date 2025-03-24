9c9081706194ad5767d00be5e0868de5e3bcf3db
Maki <maki@hotmilk.space>
2025-03-24 16:51:42 -0400

    Renive chakra from local storage

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,6 +2,10 @@
 
 import { RootStyleRegistry } from "./emotion";
 
+if (globalThis.localStorage != null) {
+	globalThis.localStorage.removeItem("chakra-ui-color-mode");
+}
+
 export function Providers({ children }: { children: React.ReactNode }) {
 	return <RootStyleRegistry>{children}</RootStyleRegistry>;
 }
